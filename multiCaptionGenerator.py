13189e01fb91b5fc1f02ae0c3c53011a9649893c
6schmalz <6schmalz@informatik.uni-hamburg.de>
2019-06-10 14:22:12 +0200

    added multiCapGen functionality - now createtakes '-f foldername/' to save into directory or create directory and then save into it

--- multiCaptionGenerator.py
+++ multiCaptionGenerator.py
@@ -1,12 +1,22 @@
+import matplotlib as mpl
+mpl.use('Pdf')
+
+import argparse
 import caption
 import os
 import subprocess
 
 
-def main():
-    subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/flosexy.jpg', '-tvr', 'random'])
-    subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/scooter.jpg', '-tvr', 'validate'])
-    subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/biketour.jpg', '-tvr', 'train'])
+def main(file_path):
+	if not os.path.isdir(file_path):
+		os.mkdir(file_path)
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/flosexy.jpg', '-tvr', 'random', '-f', file_path])
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/scooter.jpg', '-tvr', 'validate', '-f', file_path])
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/biketour.jpg', '-tvr', 'train', '-f', file_path])
 
 if __name__ == '__main__':
-    main()
+	parser = argparse.ArgumentParser(description='MultiCaptionGenerator')
+
+	parser.add_argument('--folder', '-f', default="testing/", help='folder')
+	args = parser.parse_args()
+	main(args.folder)
