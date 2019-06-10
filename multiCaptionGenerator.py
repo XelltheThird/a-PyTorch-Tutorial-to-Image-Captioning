0a3edf6369a4281d82b9d7f672002964e55666b0
NotAnton <antonwiehe@gmail.com>
2019-06-10 15:40:33 +0200

    run experiment fixed, added Visualization by integrating amazing Xell code

--- multiCaptionGenerator.py
+++ multiCaptionGenerator.py
@@ -7,16 +7,16 @@ import os
 import subprocess
 
 
-def main(file_path):
+def genVis(file_path, model_name):
 	if not os.path.isdir(file_path):
 		os.mkdir(file_path)
-	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/flosexy.jpg', '-tvr', 'random', '-f', file_path])
-	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/scooter.jpg', '-tvr', 'validate', '-f', file_path])
-	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/biketour.jpg', '-tvr', 'train', '-f', file_path])
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/flosexy.jpg', '-tvr', 'random', '-f', file_path, '-m', model_name])
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/scooter.jpg', '-tvr', 'validate', '-f', file_path, '-m', model_name])
+	subprocess.call(['python3', 'caption.py', '-i', 'VisualisationTest/biketour.jpg', '-tvr', 'train', '-f', file_path, '-m', model_name])
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='MultiCaptionGenerator')
 
 	parser.add_argument('--folder', '-f', default="testing/", help='folder')
 	args = parser.parse_args()
-	main(args.folder)
+	genVis(args.folder)
